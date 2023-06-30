7f66b64ae15cf7d549dce961c184f248646dd884
Silchas <Cartharoncrust@gmail.com>
2023-06-30 18:05:48 +0300

    minor changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,13 +16,15 @@ const Navbar = () => {
         <Login/>
         :
         <>
-            <div className="hero min-h-screen bg-base-200 flex items-center justify-center bg-lime-950">
+            <div className="hero min-h-screen bg-base-200 flex flex-col items-center justify-center bg-lime-950">
                 <div className="hero-content text-center">
                     <div className="max-w-md">
-                        <h1 className="text-4xl font-bold tracking-widest text-xl text-orange-500">Instant Chat</h1>
+                        <img src='/public/2+LETS+CHAT+BANNER+SIZE.jpg' alt=''/>
+                        <h1 className="text-6xl font-bold tracking-widest text-orange-500">Instant Chat</h1>
                         <p className="py-6">
-                        <Link to={"/Chatroom"}><a className="btn btn-ghost normal-case text-xl">Chats</a></Link>
-                        <button onClick={handleLogout} className="btn btn-ghost normal-case text-xl">Logout</button>
+                            <p>Join the conversation, meet new people and make connections in one shared platform.</p>
+                        <Link to={"/Chatroom"}><a className="btn btn-ghost normal-case text-xl text-emerald-400">Chats</a></Link>
+                        <button onClick={handleLogout} className="btn btn-ghost normal-case text-xl text-emerald-400">Logout</button>
                         </p>
                     </div>
                 </div>
--- src/pages/Chatroom.jsx
+++ src/pages/Chatroom.jsx
@@ -1,11 +1,14 @@
 import Chatbox from "../components/Chatbox"
 import SendMessage from "../components/SendMessage"
 import { Link } from "react-router-dom"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome } from "@fortawesome/free-solid-svg-icons"
 
 const Chatroom = () => {
+  const home = <FontAwesomeIcon icon={faHome} />
   return (
     <div>
-        <Link to='/'>Go home</Link>
+        <Link to='/'>{home}</Link>
         <Chatbox/>
         <SendMessage/>
     </div>
