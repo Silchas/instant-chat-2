114c5daaa46693dbd1427e53c1e94dc171e950e5
Silchas <Cartharoncrust@gmail.com>
2023-07-03 20:53:01 +0300

    other minor changes

--- src/pages/Chatroom.jsx
+++ src/pages/Chatroom.jsx
@@ -1,14 +1,13 @@
 import Chatbox from "../components/Chatbox"
 import SendMessage from "../components/SendMessage"
-import { Link } from "react-router-dom"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome } from "@fortawesome/free-solid-svg-icons"
+
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faHome } from "@fortawesome/free-solid-svg-icons"
 
 const Chatroom = () => {
-  const home = <FontAwesomeIcon icon={faHome} />
+  // const home = <FontAwesomeIcon icon={faHome} />
   return (
     <div>
-        <Link to='/'>{home}</Link>
         <Chatbox/>
         <SendMessage/>
     </div>
